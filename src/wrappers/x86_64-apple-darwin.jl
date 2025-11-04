88c99f1bab9cffa073d7ea624fad398432c6c000
jlbuild <juliabuildbot@gmail.com>
2025-11-04 00:54:25 +0000

    libxlsxwriter_jll build 1.2.3+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -3,12 +3,12 @@ export libxlsxwriter
 
 using Zlib_jll
 JLLWrappers.@generate_wrapper_header("libxlsxwriter")
-JLLWrappers.@declare_library_product(libxlsxwriter, "@rpath/libxlsxwriter.9.dylib")
+JLLWrappers.@declare_library_product(libxlsxwriter, "@rpath/libxlsxwriter.10.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(Zlib_jll)
     JLLWrappers.@init_library_product(
         libxlsxwriter,
-        "lib/libxlsxwriter.9.dylib",
+        "lib/libxlsxwriter.10.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
